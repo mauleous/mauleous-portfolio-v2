ae40621f0d7eed89f0509aa43084676b3be45dca
mauleous <mauleous@yahoo.com>
2024-01-31 23:48:25 +0200

    Adjusted the max height of Hero section to be 800px.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <section className="px-5 py-30 h-svh max-h-[1000px] w-full flex items-center justify-center">
+        <section className="px-5 py-30 h-svh max-h-[800px] w-full flex items-center justify-center">
             <div className="text-left px-8">
                 <div>Hi there,</div>
                 <div className="text-2xl">I'm Achmad</div>
