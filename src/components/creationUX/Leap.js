6805854616a66b32960a1286cb7b86498d52c426
mauleous <mauleous@yahoo.com>
2024-02-18 13:24:07 +0200

    Managed UX creation entry; added Leap and removed SARG.

---
+++ src/components/creationUX/Leap.js
@@ -0,0 +1,124 @@
+import imgPaperBracelet from "../../assets/ux/howryou/paper_bracelet.jpg"
+
+const Leap = () => {
+    return(
+        <div className="max-w-3xl h-full text-left py-8 px-12">
+            <table>
+                <tr>
+                    <td ><h3>Work type</h3></td>
+                    <td className="px-2">:</td>
+                    <td>Company work - Software UX Design and Development</td>
+                </tr>
+                <tr>
+                    <td><h3>Location</h3></td>
+                    <td className="px-2">:</td>
+                    <td>Documill</td>
+                </tr>
+                <tr>
+                    <td><h3>Year</h3></td>
+                    <td className="px-2">:</td>
+                    <td>2016 - present</td>
+                </tr>
+            </table>
+            <div className="w-full h-0.5 bg-black rounded-full my-8"></div>
+            <h2>Background</h2>
+            <p>
+                The company would like to tackle issues that happen when there are multiple people
+                involved in writing and completing complex documents. Contract documents would be a good example of this,
+                since it might involve multiple people to write them, getting approvals
+                from another parties, and might be signed by yet different set of people.
+            </p>
+            <p>
+                Documill Leap is the company's answer to enhance collaboration when preparing complex documents. 
+                It offers simultaneous multi-user text editing, custom approval process, and digital document signing.
+            </p>
+            <p>
+                In this portfolio of mine, I cannot share pictures and details of how Documill Leap was built.
+                Instead, I will describe how I contributed in different phases of the product.
+            </p>
+            <br/><br/>
+            <h2>Design</h2>
+            <p>
+                I joined the company as a UX/UI Developer. Together with stakeholders and a UX designer, we did researches and brainstormings 
+                about how to enhance the way people can work together to write documents.                
+            </p>
+            <p>
+                I helped to validate the ideas by building wireframes using Gliffy Diagram and put them into InVision,
+                as well as interactive prototypes using HTML, CSS, and JavaScript.
+            </p>
+            <table>
+                <tr>    
+                    <td><h3>Tools</h3></td>
+                    <td className="px-2">:</td>
+                    <td>Gliffy Diagram, InVision</td>
+                </tr>
+                <tr>
+                    <td><h3>Technologies</h3></td>
+                    <td className="px-2">:</td>
+                    <td>HTML, CSS, JavaScript</td>
+                </tr>
+            </table>
+            <br/><br/>
+            <h2>Development</h2>
+            <p>
+                After many of the ideas and designs were approved, a colleague and I started the actual development of Documill Leap.
+                I handled the front-end development, while my colleague built the back-end.
+            </p>
+            <p>
+                Over the time, more people were added into the project.
+                So it was the right decision to use Git from the beginning of the development.
+            </p>
+            <table>
+                <tr>
+                    <td><h3>Technologies</h3></td>
+                    <td className="px-2">:</td>
+                    <td>VueJS, HTML, CSS, Git</td>
+                </tr>
+            </table>
+            <br/><br/>
+            <h2>Becoming Development Project Manager</h2>
+            <p>
+                When the project manager decided to move to another company, 
+                I took the chance to step up and fill in the role.
+                I took the responsibility for the product documentation, managing the development tasks,
+                and ensuring that the project keeps rolling.
+                Besides that, I am also still involved in front-end development.
+                
+            </p>
+            <table>
+                <tr>
+                    <td><h3>Tools</h3></td>
+                    <td className="px-2">:</td>
+                    <td>JIRA, Confluence</td>
+                </tr>
+            </table>
+            <br/><br/>
+            <h2>Building Support Page</h2>
+            <p>
+                Just like any other products, Documill Leap also needs a Support Page;
+                a website for the end-user which explains how the product works.                
+            </p>
+            <p>
+                I researched both the concepts around writing a good support page
+                and the latest tools to build the support page. 
+                I also involved in writing the content of the support page.
+            </p>
+            <table>
+                <tr>
+                    <td><h3>Tools</h3></td>
+                    <td className="px-2">:</td>
+                    <td>Confluence, Scroll Viewport</td>
+                </tr>
+            </table>
+            <br/><br/>
+            <h2>Further iterations</h2>
+            <p>
+                Currently, I am responsible to ensure that product development keeps going,
+                process the feedback into documentations and tasks, as well as new features.
+            </p>
+            <p className="whitespace-pre"> </p>
+        </div>   
+    )
+}
+
+export default Leap;
