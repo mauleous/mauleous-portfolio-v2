1437d27fde3021cf23b66bc6a1612465fff08cac
mauleous <mauleous@yahoo.com>
2024-02-18 18:44:29 +0200

    Improved footer area

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
+import LogoHorizontal from "../assets/019_AMMH_horizontal.svg";
+
 const Footer = () => {
     return(
-        <div className="py-8">
-            <p>© 2024 AMMH.</p>
+        <div className="py-8 relative">            
+            <div className="w-full absolute flex justify-center bottom-0 opacity-10">
+                <img src={LogoHorizontal} />
+            </div>
+            <p>© 2024 AMMH | Made with React & Tailwind CSS.</p>
         </div>
     );
 }
