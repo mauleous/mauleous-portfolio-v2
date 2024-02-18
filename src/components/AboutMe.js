e3977d31826ad417270fa8efe7db5b7009fa928a
mauleous <mauleous@yahoo.com>
2024-02-18 13:24:47 +0200

    Added personal info in About Me section.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,14 +2,22 @@ import profilePicture from "../assets/20221123 - Achmad - profile picture - smal
 
 const AboutMe = () => {
     return(
-        <div id="about" className="py-32">
+        <div id="about" className="py-32 min-h-svh">
             <h1>About me</h1>
             <div className="flex py-16 flex-col md:flex-row items-center">
                 <div className="grow basis-0 px-10 py-10 flex justify-end items-center">
-                    <img className="w-60 rounded-full" 
-                        src={profilePicture}/>
+                    <div className="flex flex-col items-center">
+                        <img className="w-60 rounded-full" 
+                            src={profilePicture}/>
+                        <div className="text-center pt-6">
+                            <div>Achmad Maulana Malikul Hakim</div>
+                            <div>achmad.maulana.m.h[at]gmail.com</div>
+                            <div>@mauleous</div>
+                        </div> 
+                    </div>
+                     
                 </div>
-                <div className="grow-0 shrink-0 basis-auto w-3/4 h-1 md:w-1 md:h-80 rounded-full bg-black"></div>
+                <div className="grow-0 shrink-0 basis-auto w-3/4 h-1 md:w-1 md:h-96 rounded-full bg-black"></div>
                 <div className="grow basis-0 px-10 py-10 text-center md:text-left">
                     <div className="max-w-xs">
                         <p>
@@ -25,12 +33,13 @@ const AboutMe = () => {
                             from designing and prototyping to 
                             developing appropriate solutions.</p>
                         <p>
-                            Let's get in touch and see how we can work together!
+                            Let's get in touch and discuss how we can work together!
                         </p>
                     </div>
                     
                 </div>
             </div>
+                      
         </div>
         
     )
